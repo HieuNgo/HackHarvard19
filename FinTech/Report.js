af6ecacff1f594bc94dfe02dd9f293c300700750
Hieu Ngo (Henry) <ngohieu241@gmail.com>
2019-10-19 04:41:13 -0400

    Base start

---
+++ FinTech/Report.js
@@ -0,0 +1,103 @@
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Button,
+  Alert,
+  Text,
+  StatusBar,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+class ReportScreen extends Component{
+
+    state = {       //Step 3
+        expanded : 'none',
+    };
+    icons = {
+      icon: require('./images/weeklyReport.png')
+    }
+
+  render () {
+    let icon = this.icons[icon];
+    if(this.state.expanded){
+      icon = this.icons[icon];   //Step 4
+    } else {
+      icon = null
+    }
+    return (
+      <>
+        <View style = {styles.titleBackground}>
+        <Text style={styles.title}>Personal Sleep Advisor</Text>
+        </View>
+        <View>
+          <View style={styles.button}>
+            <Button
+              title="Daily Report"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Weekly Report"
+              onPress={() =>
+                {
+                  Alert.alert('Simple Button pressed')
+                }
+              }
+            />
+            <View style={{display: this.state.expanded}}>
+
+              <Image
+                  style={styles.buttonImage}
+                  source={icon}
+              ></Image>
+            </View>
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Monthly Report"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Overview"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+        </View>
+        </>
+
+    );
+  };
+}
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    color: "cornflowerblue",
+  },
+  titleBackground: {
+    backgroundColor: "white",
+  },
+  button: {
+      margin: 30,
+  },
+  buttonImage: {},
+});
+export default ReportScreen;
---
+++ FinTech/Settings.js
@@ -0,0 +1,115 @@
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import React, {Component} from 'react';
+import ToggleSwitch from 'toggle-switch-react-native'
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Button,
+  Alert,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+class SettingsScreen extends Component{
+  //state object
+  state = { toggle: false };
+
+  render() {
+    return (
+      <>
+        <View style = {styles.titleBackground}>
+        <Text style={styles.title}>Personal Sleep Advisor</Text>
+        </View>
+        <View>
+          <View style={styles.button}>
+            <ToggleSwitch
+              isOn={this.state.toggle}
+              onColor="green"
+              label="Notification"
+              labelStyle={{ color: "black", fontWeight: "900" }}
+              size="large"
+              onToggle={isOn => this.setState({ toggle: isOn })}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Alarm"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Smarthome Link"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Darkmode"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Font Size"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Font Color"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Calendar Background Color"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Date Format"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="Week Start"
+              onPress={() => Alert.alert('Simple Button pressed')}
+            />
+          </View>
+        </View>
+        </>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    color: "cornflowerblue",
+  },
+  titleBackground: {
+    backgroundColor: "white",
+  },
+  button: {
+      margin: 10,
+  },
+});
+
+export default SettingsScreen;
